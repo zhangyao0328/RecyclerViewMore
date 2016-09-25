f6bcc9ff0cb93ad12f7e0b417271f2e8aac3d722
zhangyao <zhangyao0328@gmail.com>
2016-09-25 21:37:28 +0800

    新建项目

---
+++ app/src/main/java/com/recyclerviewmore/fragment/FragmentThree.java
@@ -0,0 +1,22 @@
+package com.recyclerviewmore.fragment;
+
+import com.recyclerviewmore.R;
+import com.recyclerviewmore.base.BaseFrgment;
+
+/**
+ * zhangyao
+ * 16/9/24
+ * zhangyao@jiandanxinli.com
+ */
+
+public class FragmentThree extends BaseFrgment {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.fragment_three;
+    }
+
+    @Override
+    public void init() {
+
+    }
+}
---
+++ app/src/main/java/com/recyclerviewmore/widget/LoadMoreListener.java
@@ -0,0 +1,14 @@
+package com.recyclerviewmore.widget;
+
+/**
+ * zhangyao
+ * 16/9/4
+ * zhangyao@jiandanxinli.com
+ */
+public interface LoadMoreListener {
+
+    /**
+     * loading more
+     */
+    void onLoadMore();
+}
